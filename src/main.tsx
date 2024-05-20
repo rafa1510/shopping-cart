9fc0b0ec71ad37c4f49149879ccfc1f4a6f24bc1
rafa1510 <rcestti78@gmail.com>
2024-05-20 19:39:39 -0400

    feature: add initial router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import GlobalStyle from './components/GlobalStyle'
 import StyledNavbar from './components/StyledNavbar'
+import StyledHome from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <StyledHome />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
-    <StyledNavbar></StyledNavbar>
+    <StyledNavbar />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
